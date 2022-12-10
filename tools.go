cd5eedd1e0175427018d0402d0030f253f46275e
molu8bits <molu8bits@gmail.com>
2022-12-10 22:06:59 +0100

    Added basic functionality

--- tools.go
+++ tools.go
@@ -40,6 +40,26 @@ type UploadedFile struct {
 	FileSize         int64
 }
 
+// UploadOneFile is just a convenience method that calls UploadFiles, but expects only one file
+// be in the upload.
+func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return files[0], nil
+}
+
+// UploadFiles uploads one or more file to a specified directory, and gives the files a random name
+// It returns a slice containing the newly named files, the original file names, the size of the file
+// and potentially an error. If the optional last parameter is set to true, then we will not rename
+// the files, but will use the original file names.
 func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
 	renameFile := true
 	if len(rename) > 0 {
@@ -102,6 +122,7 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 					uploadedFile.NewFileName = hdr.Filename
 				}
 
+				uploadedFile.OriginalFileName = hdr.Filename
 				var outfile *os.File
 				defer outfile.Close()
 
